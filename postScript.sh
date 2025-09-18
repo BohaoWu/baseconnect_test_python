8d14a114e99df79c7d2051bfd2f7a017cc9c10e6
Ubuntu <ubuntu@ip-172-31-18-26.ap-southeast-2.compute.internal>
2025-09-18 09:35:45 +0000

    Add script in yml

--- postScript.sh
+++ postScript.sh
@@ -3,6 +3,6 @@ echo "Post script from pipeline."
 if [ -f /etc/environment ]; then
   export $(grep -v '^#' /etc/environment | xargs -r) || true
 fi
-cd news_scraping && nohup /home/ubuntu/baseconnect_test_python/venv/bin/python manage.py runserver 0.0.0.0:8000 --noreload --insecure >/dev/null 2>&1 &
+# cd news_scraping && nohup /home/ubuntu/baseconnect_test_python/venv/bin/python manage.py runserver 0.0.0.0:8000 --noreload --insecure >/dev/null 2>&1 &
 
 exit 0
