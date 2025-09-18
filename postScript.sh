a099422fb772caef4d62c10cdf6f37dcccb2abe5
Ubuntu <ubuntu@ip-172-31-18-26.ap-southeast-2.compute.internal>
2025-09-18 09:21:40 +0000

    Add exit.

--- postScript.sh
+++ postScript.sh
@@ -4,3 +4,5 @@ if [ -f /etc/environment ]; then
   export $(grep -v '^#' /etc/environment | xargs -r) || true
 fi
 cd news_scraping && nohup /home/ubuntu/baseconnect_test_python/venv/bin/python manage.py runserver 0.0.0.0:8000 --noreload --insecure >/dev/null 2>&1 &
+
+exit 0
